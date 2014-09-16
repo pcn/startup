25a51b9e703f502456a3b415e43b57f93c4d9dc4
Peter C. Norton <peter@statelessnetworks.com>
2014-09-16 09:07:31 -0400

    Go path changes.

--- mac/.bash.d/00_path.sh
+++ mac/.bash.d/00_path.sh
@@ -5,6 +5,8 @@
 if [ -z $PATH_SEEN ] ; then
     # Put /usr/local/bin and ~/bin before system defaults.
     PATH=~/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+    # Try other things I'll probably have installed by now.
     for dir in /opt/chef/bin ; do
         if [ -d $dir ] ; then
             PATH=$PATH:$dir
--- mac/.bash.d/10_go_path.sh
+++ mac/.bash.d/10_go_path.sh
@@ -2,8 +2,8 @@
 
 if [ -z $GO_PATH_SEEN ] ; then
     # Put /usr/local/bin and ~/bin before system defaults.
-    PATH=$PATH:/usr/local/go/bin
+    export GOPATH=~/dvcs/golang
+    export PATH=$PATH:/usr/local/go/bin
 
-    export PATH
     export GO_PATH_SEEN=true
 fi
