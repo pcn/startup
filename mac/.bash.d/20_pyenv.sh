bdea488ed486232fd4b1b9e5eb8b2886b2190f06
Peter Norton <peter.norton@ip-192-168-88-235.ec2.internal>
2015-06-08 16:30:09 -0400

    Add completion to pyenv and rbenv.

--- mac/.bash.d/20_pyenv.sh
+++ mac/.bash.d/20_pyenv.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+_pyenv() {
+  COMPREPLY=()
+  local word="${COMP_WORDS[COMP_CWORD]}"
+
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "$(pyenv commands)" -- "$word") )
+  else
+    local words=("${COMP_WORDS[@]}")
+    unset words[0]
+    unset words[$COMP_CWORD]
+    local completions=$(pyenv completions "${words[@]}")
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+  fi
+}
+
+
 # If we have pyenv installed initialize its
 if [ $(which pyenv | wc -l) -eq 1 ] ; then
     # Load pyenv automatically by adding
     # the following to the end of ~/.bash_profile:
     eval "$(pyenv init -)"
+    complete -F _pyenv pyenv
 fi
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
--- mac/.bash.d/20_rbenv.sh
+++ mac/.bash.d/20_rbenv.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+_rbenv() {
+  COMPREPLY=()
+  local word="${COMP_WORDS[COMP_CWORD]}"
+
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "$(rbenv commands)" -- "$word") )
+  else
+    local words=("${COMP_WORDS[@]}")
+    unset words[0]
+    unset words[$COMP_CWORD]
+    local completions=$(rbenv completions "${words[@]}")
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+  fi
+}
+
 # If we have pyenv installed initialize its
 if [ $(which rbenv | wc -l) -eq 1 ] ; then
     # Load pyenv automatically by adding
     # the following to the end of ~/.bash_profile:
     eval "$(rbenv init -)"
+    complete -F _rbenv rbenv
 fi
