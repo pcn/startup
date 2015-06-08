af3d739234d5a97b274182196005d950c562535d
Peter Norton <peter.norton@ip-192-168-88-235.ec2.internal>
2015-06-08 10:41:08 -0400

    Less noise if virtualenvwrapper isn't installed.

--- mac/.bash.d/venv.sh
+++ mac/.bash.d/venv.sh
@@ -4,16 +4,18 @@
 
 # I'll always use ~/venv as my virtualenv home
 
-export WORKON_HOME=~/venv
+if [ $(which virtualenvwrapper.sh 2>/dev/null | wc -l) -eq 1 ] ; then
+    export WORKON_HOME=~/venv
 
-if [ ! -d $WORKON_HOME ] ; then
-    mkdir -p $WORKON_HOME
+    if [ ! -d $WORKON_HOME ] ; then
+        mkdir -p $WORKON_HOME
+    fi
+
+    WRAPPER=$(which virtualenvwrapper.sh | head -1 ) # take the first one.
+
+    if [ ! -d $WORKON_HOME/python ] ; then
+        mkvirtualenv python
+    fi
+
+    source $WRAPPER
 fi
-
-WRAPPER=$(which virtualenvwrapper.sh | head -1 ) # take the first one.
-
-if [ ! -d $WORKON_HOME/python ] ; then
-    mkvirtualenv python
-fi
-
-source $WRAPPER
