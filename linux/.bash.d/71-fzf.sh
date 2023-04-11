3bf19a84bbff531b0c569f9fca9888adc3a47a86
Peter Norton <pcn@toffee.tail45a02.ts.net>
2023-04-11 09:26:33 -0400

    Continue trying fzf

---
+++ linux/.bash.d/71-fzf.sh
@@ -0,0 +1,3 @@
+# after fzf is installed as a dpkg/via apt, it isn't setup for use by default
+
+[ -e /usr/share/doc/fzf/examples/key-bindings.bash ] && . /usr/share/doc/fzf/examples/key-bindings.bash
